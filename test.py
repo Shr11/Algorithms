304521e80ab7111c7b51c01fb8213ad8e09e16b9
MageXstarLynn <shr.comp1@gmail.com>
2023-05-04 00:35:30 +0530

    making a func to reverse array

--- test.py
+++ test.py
@@ -1,9 +1,15 @@
+#reversing an array
+
 arr=[4 , 2 , 8, 6]
 n=4
-for i in range(0,int(n/2)):
+
+def revArr(arr):
+ 
+ for i in range(0,int(n/2)):
     t=arr[n-1]
     arr[n-1]=arr[i]
     arr[i]=t
     n=n-1
-print(arr)    
+ print(arr)    
    
+revArr(arr)
